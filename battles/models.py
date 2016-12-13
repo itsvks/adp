a7db0f236568d0777f44ab5e7a05c067f5fe1acf
Vikas Shil <shil.vikas@gmail.com>
2016-12-13 16:48:23 +0530

    first commit

---
+++ battles/models.py
@@ -0,0 +1,31 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Battle(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    battle_number = models.CharField(max_length=200, blank=True, null=True)
+    attacker_king = models.CharField(max_length=200, blank=True, null=True)
+    defender_king = models.CharField(max_length=200, blank=True, null=True)
+    attacker_1 = models.CharField(max_length=200, blank=True, null=True)
+    attacker_2 = models.CharField(max_length=200, blank=True, null=True)
+    attacker_3 = models.CharField(max_length=200, blank=True, null=True)
+    attacker_4 = models.CharField(max_length=200, blank=True, null=True)
+    defender_1 = models.CharField(max_length=200, blank=True, null=True)
+    defender_2 = models.CharField(max_length=200, blank=True, null=True)
+    defender_3 = models.CharField(max_length=200, blank=True, null=True)
+    defender_4 = models.CharField(max_length=200, blank=True, null=True)
+    attacker_outcome = models.CharField(max_length=200, blank=True, null=True)
+    battle_type = models.CharField(max_length=200, blank=True, null=True)
+    major_death = models.IntegerField(blank=True, null=True)
+    major_capture = models.IntegerField(blank=True, null=True)
+    attacker_size = models.CharField(max_length=200, blank=True, null=True)
+    defender_size = models.CharField(max_length=200, blank=True, null=True)
+    attacker_commander = models.CharField(max_length=200, blank=True, null=True)
+    defender_commander = models.CharField(max_length=200, blank=True, null=True)
+    summer = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    region = models.CharField(max_length=200, blank=True, null=True)
+    note = models.TextField(blank=True, null=True)
